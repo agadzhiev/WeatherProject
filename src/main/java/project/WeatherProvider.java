a84bd6a9a7d41406c72d5de44aa01588dded9fbb
agadzhiev <asad.gadzhiev07@gmail.com>
2022-02-05 17:53:01 +0300

    AcuuWeatherProvider Controller MainApp UserInterface

--- src/main/java/project/WeatherProvider.java
+++ src/main/java/project/WeatherProvider.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public interface WeatherProvider {
 
-    WeatherData getWeather(Periods periods) throws IOException;
+    void getWeather(Periods periods) throws IOException;
 
     WeatherData getAllFromDb() throws IOException;
 }
